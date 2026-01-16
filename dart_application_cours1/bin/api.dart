387a93117eed077fc24b6d86cc310a6151305263
Sacha1710 <sacha.tesson@edu.ece.fr>
2026-01-16 14:10:32 +0100

    Découvrir les APIS

---
+++ dart_application_cours1/bin/api.dart
@@ -0,0 +1,20 @@
+
+// Découvrir les APIs //
+
+void main (){
+
+//Créez une liste contenant uniquement les entiers 25, 42, 79, 12//
+  final list1 = [25, 42, 79, 12];
+
+// Triez cette liste par ordre décroissant //
+list1.sort((a, b) => b.compareTo(a));
+
+// Multipliez chaque élément de la liste par 2 avec map//
+final double = list1.map((n) => n * 2).toList();
+print('Element de la liste multiplé par 2 -> $double'); 
+
+// Utilisez la méthode where pour retourner uniquement les nombres pairs de la liste //
+final pairs = double.where((n) => n.isEven).toList();
+  print('Nombre pairs de la liste -> $pairs'); 
+
+}
