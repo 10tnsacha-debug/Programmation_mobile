d630dea9867a2166714eab7e5714dffec005d60e
Sacha1710 <sacha.tesson@edu.ece.fr>
2026-01-18 19:37:10 +0100

    réalisation etape 3 - cours 1 + correction etape 3

--- dart_application_cours1/bin/api.dart
+++ dart_application_cours1/bin/api.dart
@@ -18,3 +18,26 @@ final pairs = double.where((n) => n.isEven).toList();
   print('Nombre pairs de la liste -> $pairs'); 
 
 }
+
+// Correction //
+
+/*
+void main (){
+
+//Créez une liste contenant uniquement les entiers 25, 42, 79, 12//
+Liosrt = [25, 42, 79, 12];
+
+// Triez cette liste par ordre décroissant //
+list1.sort((a, b) => b.compareTo(a));
+
+// Multipliez chaque élément de la liste par 2 avec map//
+final double = list1.map((n) => n * 2).toList();
+print('Element de la liste multiplé par 2 -> $double'); 
+
+// Utilisez la méthode where pour retourner uniquement les nombres pairs de la liste //
+final pairs = double.where((n) => n.isEven).toList();
+  print('Nombre pairs de la liste -> $pairs'); 
+
+}
+
+ */
