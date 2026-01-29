a43d8c79b931a9093fd77430747f835a6c5fc8fd
Sacha1710 <sacha.tesson@edu.ece.fr>
2026-01-29 13:09:28 +0100

    Branche : requete -> api, asynchrome

--- cours_ece_2026/lib/model/product_notifier.dart
+++ cours_ece_2026/lib/model/product_notifier.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:formation_flutter/model/product.dart';
 
@@ -10,8 +11,29 @@ class ProductNotifier extends ChangeNotifier {
 
   Product? get product => _product;
 
-  void loadProduct() {
-    _product = generateProduct();
+  Future<void> loadProduct() async {
+    final dio = Dio();
+
+    final response = await dio.get(
+      'https://api.formation-flutter.fr/v2/getProduct',
+      queryParameters: {'barcode': '5000159484695'},
+    );
+
+    final Map<String, dynamic> data = response.data as Map<String, dynamic>;
+
+    final Map<String, dynamic> json = data['response'] as Map<String, dynamic>;
+
+    _product = Product(
+      barcode: json['barcode'] as String,
+      name: json['name'] as String?,
+      picture: (json['pictures'] as Map<String, dynamic>)['product'] as String?,
+      quantity: json['quantity'] as String?,
+      brands: (json['brands'] as List?)?.cast<String>(),
+      nutriScore: ProductNutriScore.E,
+      novaScore: ProductNovaScore.group4,
+      greenScore: ProductGreenScore.D,
+    );
+
     notifyListeners();
   }
 }
